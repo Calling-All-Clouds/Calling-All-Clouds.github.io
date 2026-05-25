a7ed16d53969b277dff978171604af6b5f9ef84a
Tony DeMarco <tony@callingallclouds.com>
2026-05-25 17:00:30 -0400

    Shorten subtitle

--- src/config.ts
+++ src/config.ts
@@ -9,8 +9,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "Calling All Clouds",
-	subtitle:
-		"Tutorials, trends, and techniques for building in the cloud. Wherever it may be.",
+	subtitle: "Blog",
 	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
