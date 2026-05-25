d27a0a975b0e3f454b73baa147210aa735940232
Tony DeMarco <tony@callingallclouds.com>
2026-05-25 17:06:50 -0400

    Replace avatar

--- src/config.ts
+++ src/config.ts
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Calling All Clouds",
 	bio: "Tutorials, trends, and techniques for building in the cloud. Wherever it may be.",
 	links: [
